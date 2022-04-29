f706c6a07005a3433b4115fbb65bf1752747736b
jalivv <shizuwei99@gmail.com>
2022-04-29 21:23:23 +0800

    中间件的使用

--- routeGroup.go
+++ routeGroup.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -9,8 +10,18 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.Use(RequestInfo())
 	user := r.Group("/user")
-	user.POST("/register", func(context *gin.Context) {
+	user.POST("/register", RegisterHandle())
+
+	if err := r.Run(); err != nil {
+		log.Fatalln(err.Error())
+		return
+	}
+}
+
+func RegisterHandle() func(context *gin.Context) {
+	return func(context *gin.Context) {
 		var user User
 		err := context.ShouldBindJSON(&user)
 		if err != nil {
@@ -19,10 +30,19 @@ func main() {
 		}
 
 		context.JSON(http.StatusOK, user)
-	})
-
-	if err := r.Run(); err != nil {
-		log.Fatalln(err.Error())
-		return
 	}
+}
+
+// 打印请求的中间价
+func RequestInfo() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		path := context.FullPath()
+		method := context.Request.Method
+		fmt.Println("请求path:", path)
+		fmt.Printf("method: %v \n", method)
+		context.Next()
+
+		fmt.Println("状态码：", context.Writer.Status())
+	}
+
 }
